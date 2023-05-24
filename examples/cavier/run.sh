19dc607c7b571544b550e7524ae9cc50b591aae7
Johann Schwabe <johann.schwabe@uzh.ch>
2023-05-24 16:39:41 +0200

    fixed tpch3Q2

--- examples/cavier/run.sh
+++ examples/cavier/run.sh
@@ -10,7 +10,7 @@ for file in cavier/config/$1/*; do
   echo ../cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
   ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
 
-  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/${filename}_BATCH_1000; \
+  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000; \
   g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 || exit 1; \
   if [ $2 = "exe" ]; then
     ./bin/$1/CAVIER_${filename}_BATCH_1000 --no-output
