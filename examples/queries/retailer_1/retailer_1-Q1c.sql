07fbf9d0e1d722a484061159a4285580b819623c
Johann Schwabe <johann.schwabe@uzh.ch>
2023-05-26 10:25:41 +0200

    fixed retailer_Q1c

---
+++ examples/queries/retailer_1/retailer_1-Q1c.sql
@@ -0,0 +1,24 @@
+IMPORT DTREE FROM FILE 'retailer_1-Q1c.txt';
+
+CREATE DISTRIBUTED TYPE RingFactorizedRelation
+FROM FILE 'ring/ring_factorized.hpp'
+WITH PARAMETER SCHEMA (dynamic_min);
+
+CREATE STREAM q2 (
+    locn int,
+    dateid int,
+    ksn int,
+    rain int,
+    maxtemp int,
+    zip int
+) FROM FILE './datasets/retailer/q2.tbl' LINE DELIMITED CSV(delimiter := '|');
+
+CREATE STREAM Item(ksn int, subcategory int, category int, categoryCluster int, prize double)
+FROM FILE './datasets/retailer/Item.tbl' LINE DELIMITED CSV(delimiter := '|');
+
+SELECT SUM(
+    [lift<0>: RingFactorizedRelation<[0,int]>](ksn) *
+    [lift<1>: RingFactorizedRelation<[1,int, int, int, int]>](locn,dateid,rain,zip) *
+    [lift<8>: RingFactorizedRelation<[8,int]>](category)
+)
+FROM q2 NATURAL JOIN Item
