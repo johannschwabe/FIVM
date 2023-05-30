20f6f229fa84c3926955375a268d10e452b1eba9
Johann Schwabe <johann.schwabe@uzh.ch>
2023-05-30 10:47:04 +0200

    TPCH 4 compiling

---
+++ examples/queries/tpch_4/tpch_4*1-Q1a.sql
@@ -0,0 +1,85 @@
+IMPORT DTREE FROM FILE 'tpch_4-Q1a.txt';
+
+CREATE DISTRIBUTED TYPE RingFactorizedRelation
+FROM FILE 'ring/ring_factorized.hpp'
+WITH PARAMETER SCHEMA (dynamic_min);
+
+CREATE STREAM lineitem (
+        orderkey         INT,
+        partkey          INT,
+        suppkey          INT,
+        l_linenumber     INT,
+        l_quantity       DECIMAL,
+        l_extendedprice  DECIMAL,
+        l_discount       DECIMAL,
+        l_tax            DECIMAL,
+        l_returnflag     CHAR(1),
+        l_linestatus     CHAR(1),
+        l_shipdate       CHAR(10),
+        l_commitdate     CHAR(10),
+        l_receiptdate    CHAR(10),
+        l_shipinstruct   CHAR(25),
+        l_shipmode       CHAR(10),
+        l_comment        VARCHAR(44)
+    )
+  FROM FILE './datasets/tpch/lineitem.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+
+CREATE STREAM orders (
+        orderkey         INT,
+        custkey          INT,
+        o_orderstatus    CHAR(1),
+        o_totalprice     DECIMAL,
+        o_orderdate      CHAR(10),
+        o_orderpriority  CHAR(15),
+        o_clerk          CHAR(15),
+        o_shippriority   INT,
+        o_comment        VARCHAR(79)
+    )
+  FROM FILE './datasets/tpch/orders.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+CREATE STREAM customer (
+        custkey        INT,
+        c_name         VARCHAR(25),
+        c_address      VARCHAR(40),
+        nationkey    INT,
+        c_phone        CHAR(15),
+        c_acctbal      DECIMAL,
+        c_mktsegment   CHAR(10),
+        c_comment      VARCHAR(117)
+    )
+  FROM FILE './datasets/tpch/customer.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+CREATE STREAM partsupp (
+        partkey         INT,
+        suppkey         INT,
+        ps_availqty     INT,
+        ps_supplycost   DECIMAL,
+        ps_comment      VARCHAR(199)
+    )
+  FROM FILE './datasets/tpch/partsupp.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+CREATE STREAM supplier (
+        suppkey        INT,
+        s_name         CHAR(25),
+        s_address      VARCHAR(40),
+        nationkey    INT,
+        s_phone        CHAR(15),
+        s_acctbal      DECIMAL,
+        s_comment      VARCHAR(101)
+    )
+  FROM FILE './datasets/tpch/supplier.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+SELECT SUM(
+    [lift<0>: RingFactorizedRelation<[0, INT]>](orderkey) *
+    [lift<1>: RingFactorizedRelation<[1, INT]>](partkey) *
+    [lift<2>: RingFactorizedRelation<[2, INT]>](suppkey) *
+    [lift<19>: RingFactorizedRelation<[19, INT]>](nationkey) *
+    [lift<25>: RingFactorizedRelation<[25, INT]>](custkey)
+   )
+FROM lineitem NATURAL JOIN orders NATURAL JOIN customer NATURAL JOIN partsupp NATURAL JOIN supplier;
---
+++ examples/queries/tpch_4/tpch_4*1-Q3.sql
@@ -0,0 +1,41 @@
+IMPORT DTREE FROM FILE 'tpch_4-Q3.txt';
+
+CREATE DISTRIBUTED TYPE RingFactorizedRelation
+FROM FILE 'ring/ring_factorized.hpp'
+WITH PARAMETER SCHEMA (dynamic_min);
+
+CREATE STREAM orders (
+        orderkey         INT,
+        custkey          INT,
+        o_orderstatus    CHAR(1),
+        o_totalprice     DECIMAL,
+        o_orderdate      CHAR(10),
+        o_orderpriority  CHAR(15),
+        o_clerk          CHAR(15),
+        o_shippriority   INT,
+        o_comment        VARCHAR(79)
+    )
+  FROM FILE './datasets/tpch/orders.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+
+CREATE STREAM customer (
+        custkey        INT,
+        c_name         VARCHAR(25),
+        c_address      VARCHAR(40),
+        nationkey      INT,
+        c_phone        CHAR(15),
+        c_acctbal      DECIMAL,
+        c_mktsegment   CHAR(10),
+        c_comment      VARCHAR(117)
+    )
+  FROM FILE './datasets/tpch/customer.csv'
+  LINE DELIMITED CSV (delimiter := '|');
+
+
+SELECT SUM(
+    [lift<0>: RingFactorizedRelation<[0, INT]>](custkey) *
+    [lift<1>: RingFactorizedRelation<[1, INT]>](orderkey) *
+    [lift<8>: RingFactorizedRelation<[8, INT]>](nationkey)
+   )
+FROM orders NATURAL JOIN customer;
