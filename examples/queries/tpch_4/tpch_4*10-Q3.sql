335f104c7297dd34d88666311e47b4fd246924c0
Johann Schwabe <johann.schwabe@uzh.ch>
2023-05-30 12:06:45 +0200

    added Scale 10 to TPCH4

--- examples/queries/tpch_4/tpch_4*10-Q3.sql
+++ examples/queries/tpch_4/tpch_4*10-Q3.sql
@@ -15,7 +15,7 @@ CREATE STREAM orders (
         o_shippriority   INT,
         o_comment        VARCHAR(79)
     )
-  FROM FILE './datasets/tpch1/orders.csv'
+  FROM FILE './datasets/tpch10/orders.csv'
   LINE DELIMITED CSV (delimiter := '|');
 
 
@@ -29,7 +29,7 @@ CREATE STREAM customer (
         c_mktsegment   CHAR(10),
         c_comment      VARCHAR(117)
     )
-  FROM FILE './datasets/tpch1/customer.csv'
+  FROM FILE './datasets/tpch10/customer.csv'
   LINE DELIMITED CSV (delimiter := '|');
 
 
