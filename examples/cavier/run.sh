63ea10bebfb46b878a0168486237e6bce6858f73
Johann Schwabe <johann.schwabe@uzh.ch>
2023-05-31 18:51:11 +0200

    local install success

--- examples/cavier/run.sh
+++ examples/cavier/run.sh
@@ -11,7 +11,7 @@ for file in cavier/config/$1/*; do
   ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
 
   echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include/tlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000; \
-  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include/tlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 || exit 1; \
+  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 || exit 1; \
   if [ $2 = "exe" ]; then
     ./bin/$1/CAVIER_${filename}_BATCH_1000
   fi
