9b7fb645171a2a5501a79f5db18c1cfa75dda7ff
Johann Schwabe <johann.schwabe@uzh.ch>
2023-06-12 09:44:47 +0200

    fixed flags

--- examples/cavier/run.sh
+++ examples/cavier/run.sh
@@ -1,4 +1,4 @@
-g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wunused-local-typedef src/result_enumeration.cpp -o cavier/app_generator || exit 1;
+g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/result_enumeration.cpp -o cavier/app_generator || exit 1;
 mkdir -p "cavier/application/$1"
 mkdir -p "bin/$1"
 
@@ -10,8 +10,8 @@ for file in cavier/config/$1/*; do
   echo ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
   ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
   customhashmapargs=($(zsh custom_hashmap.zsh "$filename"))
-  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
-  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wunused-local-typedef src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
+  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
+  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
   if [ $2 = "exe" ]; then
     ./bin/$1/CAVIER_${filename}_BATCH_1000
   fi
