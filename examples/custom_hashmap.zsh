b406d3f5951f7c2cbea0e7acc0e87174b375ebb1
Johann Schwabe <johann.schwabe@uzh.ch>
2023-06-13 10:38:32 +0200

    fixed custom hashmap sizes on unordered data

--- examples/custom_hashmap.zsh
+++ examples/custom_hashmap.zsh
@@ -9,14 +9,16 @@ while IFS=',' read -r query regular size; do
   max_regular[$query]=$regular
   max_size[$query]=$size
 done < CustomHashmapSizes.txt
-# Retrieve the macros for the provided query
-# Check if the string contains an asterisk
-if [[ $1 == *\** ]]; then
+if [[ $1 == *\*_unordered* ]]; then
+    # Split at '*_unordered'
+    first_part=${1%%\*_unordered*}
+    second_part=${1##*\*_unordered}
+elif [[ $1 == *\** ]]; then
     # Split at the asterisk
     first_part=${1%%\**}
     second_part=${1##*\*}
 else
-    # If there is no asterisk, use the original string as the first part and '1' as the second part
+    # If there is no asterisk or '*_unordered', use the original string as the first part and '1' as the second part
     first_part=$1
     second_part=1
 fi
