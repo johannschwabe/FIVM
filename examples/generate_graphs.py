9001e5766a9620b08b216e921160ca4ec1363aa4
Johann Schwabe <johann.schwabe@uzh.ch>
2023-06-18 19:04:19 +0200

    nicer legends

--- examples/generate_graphs.py
+++ examples/generate_graphs.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import matplotlib.patches as patches
+import matplotlib.colors as mcolors
 
 # Read the text file
 with open('output/output.txt', 'r') as f:
@@ -33,7 +34,6 @@ df['query_root'] = df['query_root'].str.strip()
 output_dir = 'viz'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-
 # Loop through unique names
 for unique_name in df['name'].unique():
     # Filter the data for the current name
@@ -44,8 +44,9 @@ for unique_name in df['name'].unique():
 
     # Unique executors
     executors = name_data['executor'].unique()
-    # Define colors for executors
-    color_map = {executor: color for executor, color in zip(executors, ['blue', 'orange'])}
+
+    # Colors for executors
+    base_colors = ['blue', 'orange']
 
     # Width of a bar
     bar_width = 0.35
@@ -58,6 +59,9 @@ for unique_name in df['name'].unique():
     # Keep track of x-ticks positions
     x_ticks = []
 
+    combination_legend_handles = []
+    handled_combinations = set()  # To keep track of handled combinations
+
     # Loop through query roots
     for qr_idx, query_root in enumerate(sorted(query_roots)):
         # Number of queries within the current query root
@@ -68,20 +72,30 @@ for unique_name in df['name'].unique():
             length = len(name_data[(name_data['query_root'] == query_root) & (name_data['executor'] == executor)])/len(version_data)
             positions = start_pos + np.arange(length) * (bar_width + bar_distance)
             first = True
-            for version in version_data:
+            base_color = mcolors.hex2color(base_colors[ex_idx])
+            base_hsv = mcolors.rgb_to_hsv(base_color)
+            for version_idx, version in enumerate(version_data):
+                shade = base_hsv.copy()
+                shade[2] = max(0.1, shade[2] - version_idx * 0.4)
+                color = mcolors.hsv_to_rgb(shade)
                 query_data = name_data[(name_data['query_root'] == query_root) & (name_data['executor'] == executor) & (name_data['dataset'] == version)]
-                heights = query_data['update_time'].values
-
-                # Positions calculation
+                heights = query_data['update_time'].values / 1000
 
                 # Plot bars for each query within a query_root
-                bars = ax.bar(positions, heights, width=bar_width, color=color_map[executor], alpha=0.6)
+                bars = ax.bar(positions, heights, width=bar_width, color=color, alpha=1)
                 if first:
                     first = False
                     for bar, label in zip(bars, query_data['query']):
                         height = bar.get_height()
                         ax.text(bar.get_x() + bar.get_width() / 2, height, label,
                                 ha='center', va='bottom', rotation=90, fontsize=8)
+
+                # Add patch for legend if combination is not handled
+                combination = f'{executor}{version}'
+                if combination not in handled_combinations:
+                    combination_patch = patches.Patch(color=color, label=combination)
+                    combination_legend_handles.append(combination_patch)
+                    handled_combinations.add(combination)
 
             # Updating the start position for the next executor
             start_pos = positions[-1] + bar_width + bar_distance
@@ -90,25 +104,13 @@ for unique_name in df['name'].unique():
         # Updating the start position for the next query root group and storing x-ticks
         start_pos += bar_width * 2
 
-    legend_handles = []
-    for ex_idx, executor in enumerate(executors):
-        version_data = name_data[name_data['executor'] == executor]['dataset'].unique()
-        color = color_map[executor]
-        n_versions = len(version_data)
-        for version_idx, version in enumerate(version_data):
-            # manually adjust alpha to simulate lightening effect
-            alpha = 1 - version_idx * 0.3
-            alpha = max(0.3, alpha)  # Keep alpha above 0.3 to avoid too much transparency
-            patch = patches.Patch(color=color, alpha=alpha, label=f'{executor} - {version}')
-            legend_handles.append(patch)
-
     # Set x-ticks to the middle of each query root group
     ax.set_xticks(x_ticks)
     ax.set_xticklabels(sorted(query_roots))
 
     ax.set_xlabel('Query Root')
-    ax.set_ylabel('Update Time (ms)')
-    ax.legend(handles=legend_handles, loc='upper left', bbox_to_anchor=(1, 1), title="Executor - Version")
+    ax.set_ylabel('Update Time (s)')
+    ax.legend(handles=combination_legend_handles, loc='upper left', bbox_to_anchor=(1, 1), title="Executor - Version")
 
     # Save plot
     plt.savefig(os.path.join(output_dir, f'{unique_name}_plot.png'), bbox_inches='tight')
