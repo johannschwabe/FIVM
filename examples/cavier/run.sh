665ca3f696793eb9509a51076d10d8f0498d78c4
Johann Schwabe <johann.schwabe@uzh.ch>
2023-06-25 23:44:53 +0200

    custom Hashmap

--- examples/cavier/run.sh
+++ examples/cavier/run.sh
@@ -2,16 +2,17 @@ g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic src/result_enum
 mkdir -p "cavier/application/$1"
 mkdir -p "bin/$1"
 
-../bin/run_backend.sh --batch -o cavier/cpp/$1/$1.hpp cavier/m3/$1/$1_BATCH.m3
 
 for file in cavier/config/$1/*; do
   filename=$(basename "$file" .txt)
   echo $filename
   echo ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
   ./cavier/app_generator "$file" "cavier/application/$1/$filename.hpp"
+  ../bin/run_backend.sh --batch -o "cavier/cpp/$1/$filename.hpp" "cavier/m3/$1/$filename.m3"
+
   customhashmapargs=($(zsh custom_hashmap.zsh "$filename"))
-  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
-  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$1.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
+  echo g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$filename.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
+  g++ -O3 -DNDEBUG -Wall -Wno-unused-variable -std=c++17 -pedantic -Wno-unused-local-typedefs src/main.cpp -I ../backend/lib -I src -I src/lib -I src/TLXInstall/include -L src/TLXInstall/lib -ltlx -DBATCH_SIZE=1000 -include cavier/cpp/$1/$filename.hpp -include cavier/application/$1/$filename.hpp -o bin/$1/CAVIER_${filename}_BATCH_1000 "${customhashmapargs[@]}" || exit 1;
   if [ $2 = "exe" ]; then
     echo ./bin/$1/CAVIER_${filename}_BATCH_1000
     ./bin/$1/CAVIER_${filename}_BATCH_1000
--- examples/generate_graphs.py
+++ examples/generate_graphs.py
@@ -4,11 +4,13 @@ import os
 import numpy as np
 import matplotlib.patches as patches
 import matplotlib.colors as mcolors
+import re
 
 # Read the text file
 with open('output/output.txt', 'r') as f:
     lines = f.readlines()
 
+root_regex = r'(\D*\d+)'
 # Parse the data
 data = []
 for line in lines:
@@ -17,116 +19,98 @@ for line in lines:
     base = [name, dataset, all_relations]
     for res in rest:
         subname, maptype, update_time, enumeration_time, count, varnames, relations = res.split('|')
+        splitted_var_names = [x.strip() for x in varnames.lower().split(',')]
+        sorted_var_names = ','.join(sorted(splitted_var_names)).replace(' ', '').replace('_', '')
+        # extract the query root using the regex r'(\D*\d+)' and strip the whitespaces
+        query_root = re.search(root_regex, subname).group(1).strip()
 
+        query_root_unique = f"{query_root} - {len(sorted_var_names.split(',')):02d}"
         data.append(
-            base + [executor, subname, maptype, int(update_time), int(enumeration_time), int(count), varnames, relations])
+            base + [executor, subname.strip(), maptype, int(update_time), int(enumeration_time), int(count),
+                    sorted_var_names, relations, query_root, query_root_unique])
 
 # Create a DataFrame
 df = pd.DataFrame(data)
 df.columns = ['name', 'dataset', 'all Relations', 'executor', 'query', 'maptype', 'update_time', 'enumeration time',
-              'nr tuples', 'free variables', 'relations']
+              'nr tuples', 'free variables', 'relations', 'query_root', 'query_root_unique']
 
-# Extract root query name
-df['query_root'] = df['query'].str.extract(r'(\D*\d+)')
-df['query_unique_root'] = df['query_root'].str.strip() + ' - ' + df['free variables'].str.strip()
 
 # Directory to save plots
 output_dir = 'viz'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# Loop through unique names
-for unique_name in df['name'].unique():
-    # Filter the data for the current name
-    name_data = df[df['name'] == unique_name]
+# Colors for executors
 
-    # Unique query roots
-    query_roots = name_data['query_unique_root'].unique()
-
-    # Unique executors
-    executors = name_data['executor'].unique()
-
-    # Colors for executors
-    base_colors = ['blue', 'orange']
-
-    # Width of a bar
-    bar_width = 0.35
-
+base_colors = {'CAVIER': 'blue', "FIVM": 'orange'}
+# Unique executors
+executors = df['executor'].unique()
+# Width of a bar
+bar_width = 0.35
+bar_distance = 0.05
+for name in df['name'].unique():
+    name_data = df[df['name'] == name]
     fig, axes = plt.subplots(1, 2, figsize=(12, 6), sharey=True)
 
-    bar_distance = 0.05
+    combination_legend_handles = []
+    handled_combinations = set()  # To keep track of handled combinations
+
     for idx, metric in enumerate(['update_time', 'enumeration time']):
         ax = axes[idx]
-
-        # Start position for the first bar
-        start_pos = 0
-        # Keep track of x-ticks positions
         x_ticks = []
+        x_tick_labels = []
+        for version_idx, dataset_version in enumerate(name_data['dataset'].unique()):
+            dataset_data = name_data[name_data['dataset'] == dataset_version]
+            # Start position for the first bar
+            start_pos = 0
+            last_post = 0
+            length_unique = len(dataset_data['query_root_unique'].unique())
+            length_non_unique = len(dataset_data['query_root'].unique())
+            for query_root_unique in sorted(dataset_data['query_root_unique'].unique()):
+                query_root_unique_data = dataset_data[dataset_data['query_root_unique'] == query_root_unique]
+                for query in query_root_unique_data['query'].unique():
+                    query_data = query_root_unique_data[query_root_unique_data['query'] == query]
+                    for executor in query_data['executor'].unique():
+                        exucutor_data = query_data[query_data['executor'] == executor]
+                        # Calculate the average and standard deviation for the group
+                        avg_height = exucutor_data[metric].mean() / 1000
+                        std_height = exucutor_data[metric].std() / 1000
 
-        combination_legend_handles = []
-        handled_combinations = set()  # To keep track of handled combinations
+                        base_color = mcolors.hex2color(base_colors[executor])
+                        base_hsv = mcolors.rgb_to_hsv(base_color)
+                        shade = base_hsv.copy()
+                        shade[2] = max(0.1, shade[2] - version_idx * 0.4)
+                        color = mcolors.hsv_to_rgb(shade)
 
-        # Loop through query roots
-        for qr_idx, query_root in enumerate(sorted(query_roots)):
-            # Number of queries within the current query root
-            start = start_pos
-            # Loop through executors
-            for ex_idx, executor in enumerate(executors):
-                version_data = name_data[(name_data['query_unique_root'] == query_root) & (name_data['executor'] == executor)]['dataset'].unique()
-                if len(version_data) == 0:
-                    continue
-                length = len(name_data[(name_data['query_unique_root'] == query_root) & (name_data['executor'] == executor)]) / len(version_data)
-                positions = start_pos + np.arange(length) * (bar_width + bar_distance)
-                first = True
-                base_color = mcolors.hex2color(base_colors[ex_idx])
-                base_hsv = mcolors.rgb_to_hsv(base_color)
-                for version_idx, version in enumerate(version_data):
-                    shade = base_hsv.copy()
-                    shade[2] = max(0.1, shade[2] - version_idx * 0.4)
-                    color = mcolors.hsv_to_rgb(shade)
-                    group = name_data[(name_data['query_unique_root'] == query_root) & (name_data['executor'] == executor) & (name_data['dataset'] == version)]
+                        # Plot bars with error bars
+                        bar = ax.bar(start_pos, avg_height, width=bar_width, color=color, alpha=1, yerr=std_height)
+                        if version_idx == 0:
+                            query_name = query if length_unique == length_non_unique else f"{query} - {len(exucutor_data.iloc[0]['free variables'].split(','))}"
+                            ax.text(start_pos, (avg_height + std_height) * 1.01, query_name, ha='center', va='bottom',
+                                    rotation=90, fontsize=8)
 
-                    # Calculate the average and standard deviation for the group
-                    avg_height = group[metric].mean() / 1000
-                    std_height = group[metric].std() / 1000
-                    position = start_pos + (bar_width + bar_distance) * version_idx
+                        start_pos += (bar_width + bar_distance)
+                        combination = f'{executor} - {dataset_version}'
+                        if combination not in handled_combinations:
+                            combination_patch = patches.Patch(color=color, label=combination)
+                            combination_legend_handles.append(combination_patch)
+                            handled_combinations.add(combination)
 
-                    # Plot bars with error bars
-                    bar = ax.bar(position, avg_height, width=bar_width, color=color, alpha=1, yerr=std_height)
-                    if first:
-                        first = False
-                        name = name_data[(name_data['query_unique_root'] == query_root)].iloc[0]['query']
-                        ax.text(bar[0].get_x() + bar[0].get_width() / 2, avg_height, name,
-                                ha='center', va='bottom', rotation=90, fontsize=8)
-
-
-                    # Add patch for legend if combination is not handled
-                    combination = f'{executor} - {version}'
-                    if combination not in handled_combinations:
-                        combination_patch = patches.Patch(color=color, label=combination)
-                        combination_legend_handles.append(combination_patch)
-                        handled_combinations.add(combination)
-
-                # Updating the start position for the next executor
-                start_pos = positions[-1] + bar_width + bar_distance
-
-            x_ticks.append((start_pos + start) / 2 - bar_width / 2 - bar_distance / 2)
-            # Updating the start position for the next query root group and storing x-ticks
-            start_pos += bar_width * 2
-
-        # Set x-ticks to the middle of each query root group
-        ax.set_xticks(x_ticks)
-        ax.set_xticklabels(sorted(query_roots))
+                x_ticks.append((start_pos + last_post) / 2 - bar_width / 2 - bar_distance / 2)
+                x_tick_labels.append(query_root_unique_data.iloc[0]['query_root'])
+                start_pos += (bar_width + bar_distance)
+                last_post = start_pos
 
         ax.set_xlabel('Query')
         ax.set_ylabel(f'{metric.replace("_", " ").capitalize()} (s)')
-
-        # Add legend to the first plot only
+        ax.set_xticks(x_ticks)
+        ax.set_xticklabels(x_tick_labels, rotation=90)
         if idx == 1:
             ax.legend(handles=combination_legend_handles, loc='upper right', bbox_to_anchor=(1, 1), title="Executor - Version")
 
-    # Save plot
     plt.tight_layout(rect=[0, 0, 1, 0.95])
-    fig.suptitle(unique_name)
-    plt.savefig(os.path.join(output_dir, f'{unique_name}_plot.png'), bbox_inches='tight', dpi=300)
-    plt.close()
+    fig.suptitle(name)
+    # plt.show()
+    plt.savefig(os.path.join(output_dir, f'{name}_plot.png'), bbox_inches='tight', dpi=300)
+
+    print("gugus")
